4f35fb08caa165ff2c6f52ffd3fd3592ee07863a
Erasmo <erasmolpa@gmail.com>
2023-11-06 11:57:01 +0000

    Delete run.sh

--- run.sh
+++
@@ -1,5 +0,0 @@
-export ACCESS_TOKEN=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
-ORGANIZATION=erasmolpaorg
-REPO=kubernetes
-# e.g. git@github.com:docker/cli.git
-github-backup $ORGANIZATION -t $ACCESS_TOKEN -o . -i -l DEBUG --all
