93b535936510af2b20ea82095455f85602925fbb
Erasmo Dominguez Jimenez <edominguez@ustspain.es>
2023-11-20 16:34:04 +0000

    script

--- smoke_test.ps1
+++ smoke_test.ps1
@@ -1,20 +1,10 @@
 $orgName = 'erasmolpaorg'
-$accessTokenRepo = $env:GITHUB_ACCESS_TOKEN
+$accessToken = $env:GITHUB_ACCESS_TOKEN
 $outputDir = './'
 $repoNamesRepo = "test"
-$accessTokenProject = $env:GITHUB_ACCESS_TOKEN
 $projectID = '3'
 
-$backupScriptRepo = ".\github_tool\gh_repo_backup\backup.py"
+python .\github_tool\gh_repo_backup\backup.py -o $orgName -t $accessToken -d $outputDir -r $repoNamesRepo
 
-$repoBackupCommand = "python $backupScriptRepo -o '$orgName' -t $accessTokenRepo -d '$outputDir' -r '$repoNamesRepo'"
+python .\github_tool\gh_project_backup\projects.py -o $orgName -t $accessToken -d $outputDir -p $projectID
 
-Invoke-Expression -Command $repoBackupCommand
-
-
-$backupScriptProject = ".\github_tool\gh_project_backup\projects.py"
-
-$projectBackupCommand = "python $backupScriptProject -o '$orgName' -t $accessTokenProject -d '$outputDir' -p '$projectID'"
-
-
-Invoke-Expression -Command $projectBackupCommand
