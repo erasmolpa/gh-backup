399eb8832a003406d48ec7659f8735f4e85b3c57
Erasmo Dominguez Jimenez <edominguez@ustspain.es>
2023-11-21 06:19:37 +0000

    smoke

--- smoke_test.ps1
+++ smoke_test.ps1
@@ -6,5 +6,5 @@ $projectID = '3'
 
 python .\github_tool\gh_repo_backup\backup.py -o $orgName -t $accessToken -d $outputDir -r $repoNamesRepo
 
-python .\github_tool\gh_project_backup\projects.py -o $orgName -t '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' -d $outputDir -p $projectID
+python .\github_tool\gh_project_backup\projects.py -o $orgName -t $accessToken -d $outputDir -p $projectID
 
