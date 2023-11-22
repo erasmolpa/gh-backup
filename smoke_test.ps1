dec8d7a115c58a10192992a7cbf03df2bb2467f2
Erasmo Dominguez Jimenez <edominguez@ustspain.es>
2023-11-22 07:13:01 +0000

    repo restored

--- smoke_test.ps1
+++ smoke_test.ps1
@@ -3,7 +3,7 @@ $backupDir = './backups'
 $repoNamesRepo = "repo-created-with-terraform"
 #$projectID = '3'
 
-$restoreDir = './repo_dir/test/'
+$restoreDir = './backups/erasmolpaorg/repo-created-with-terraform_2023-11-22.zip'
 python .\github_tool\gh_repo_backup\backup.py -o $orgName -t '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' -d $backupDir -r $repoNamesRepo -rc -pb
 
 #python .\github_tool\gh_project_backup\projects.py -o $orgName -t '${env:GITHUB_ACCESS_TOKEN}' -d $outputDir -p $projectID
